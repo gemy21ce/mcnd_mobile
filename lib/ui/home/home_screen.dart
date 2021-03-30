20dbbc0f2723bfcb275a656f3e5bed5cf40f76f4
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-03-30 21:25:01 +0200

    add bottom nav

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:mcnd_mobile/ui/prayer_times/prayer_times_page.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,6 +10,23 @@ class HomeScreen extends StatelessWidget {
         title: Text("MCND Mosque"),
       ),
       body: PrayerTimesPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.clockOutline),
+            label: "Prayer Times",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.compass),
+            label: "Compass",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.newspaper),
+            label: "News",
+          ),
+        ],
+      ),
     );
   }
 }
