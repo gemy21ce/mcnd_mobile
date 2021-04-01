7453dd96c0ad4e56431c31267a99ee1933bfaf5b
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-01 20:58:42 +0200

    mcnd_api_test.dart
    + changed api_prayer_time.dart to use freezed

--- lib/data/models/api/api_prayer_time.dart
+++ lib/data/models/api/api_prayer_time.dart
@@ -1,62 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:mcnd_mobile/data/models/mcnd_datetime_converter.dart';
 
+part 'api_prayer_time.freezed.dart';
 part 'api_prayer_time.g.dart';
 
-@JsonSerializable()
-@McndTimeConverter()
-class ApiPrayerTime {
-  @JsonKey(name: "d_date")
-  @McndDateConverter()
-  final DateTime date;
-
-  @JsonKey(name: "fajr_begins")
-  final DateTime fajrAzan;
-  @JsonKey(name: "fajr_jamah")
-  final DateTime fajrIqamah;
-
-  @JsonKey(name: "sunrise")
-  final DateTime sunrise;
-
-  @JsonKey(name: "zuhr_begins")
-  final DateTime zuhrAzan;
-  @JsonKey(name: "zuhr_jamah")
-  final DateTime zuhrIqamah;
-
-  @JsonKey(name: "asr_mithl_1")
-  final DateTime asrAzan;
-  @JsonKey(name: "asr_mithl_2")
-  final DateTime asrAzan2;
-  @JsonKey(name: "asr_jamah")
-  final DateTime asrIqamah;
-
-  @JsonKey(name: "maghrib_begins")
-  final DateTime maghribAzan;
-  @JsonKey(name: "maghrib_jamah")
-  final DateTime maghribIqamah;
-
-  @JsonKey(name: "isha_begins")
-  final DateTime ishaAzan;
-  @JsonKey(name: "isha_jamah")
-  final DateTime ishaIqamah;
-
-  ApiPrayerTime({
-    required this.date,
-    required this.fajrAzan,
-    required this.fajrIqamah,
-    required this.sunrise,
-    required this.zuhrAzan,
-    required this.zuhrIqamah,
-    required this.asrAzan,
-    required this.asrAzan2,
-    required this.asrIqamah,
-    required this.maghribAzan,
-    required this.maghribIqamah,
-    required this.ishaAzan,
-    required this.ishaIqamah,
-  });
+@freezed
+class ApiPrayerTime with _$ApiPrayerTime {
+  @McndTimeConverter()
+  factory ApiPrayerTime({
+    @JsonKey(name: "d_date") @McndDateConverter() required DateTime date,
+    @JsonKey(name: "fajr_begins") required DateTime fajrAzan,
+    @JsonKey(name: "fajr_jamah") required DateTime fajrIqamah,
+    @JsonKey(name: "sunrise") required DateTime sunrise,
+    @JsonKey(name: "zuhr_begins") required DateTime zuhrAzan,
+    @JsonKey(name: "zuhr_jamah") required DateTime zuhrIqamah,
+    @JsonKey(name: "asr_mithl_1") required DateTime asrAzan,
+    @JsonKey(name: "asr_mithl_2") required DateTime asrAzan2,
+    @JsonKey(name: "asr_jamah") required DateTime asrIqamah,
+    @JsonKey(name: "maghrib_begins") required DateTime maghribAzan,
+    @JsonKey(name: "maghrib_jamah") required DateTime maghribIqamah,
+    @JsonKey(name: "isha_begins") required DateTime ishaAzan,
+    @JsonKey(name: "isha_jamah") required DateTime ishaIqamah,
+  }) = _ApiPrayerTime;
 
   factory ApiPrayerTime.fromJson(Map<String, dynamic> json) =>
       _$ApiPrayerTimeFromJson(json);
-  Map<String, dynamic> toJson() => _$ApiPrayerTimeToJson(this);
 }
---
+++ test/data/network/mcnd_api_test.dart
@@ -0,0 +1,135 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mcnd_mobile/data/models/api/api_prayer_time.dart';
+import 'package:mcnd_mobile/data/models/api/prayer_time_filter.dart';
+import 'package:mcnd_mobile/data/models/mappers/mapper.dart';
+import 'package:mcnd_mobile/data/network/mcnd_api.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'mcnd_api_test.mocks.dart';
+
+extension _WhenDio on MockDio {
+  PostExpectation<Future<Response<dynamic>>> whenAnyGet() {
+    final res = when(this.get(
+      any,
+      queryParameters: anyNamed("queryParameters"),
+      options: anyNamed("options"),
+      cancelToken: anyNamed("cancelToken"),
+      onReceiveProgress: anyNamed("onReceiveProgress"),
+    ));
+
+    return res;
+  }
+}
+
+@GenerateMocks([Mapper, Dio])
+void main() {
+  test(
+      "when McndApi called with TODAY filter it adds it to the arguments and receives correct result",
+      () async {
+    final dio = MockDio();
+    final mapper = MockMapper();
+    final api = McndApi(dio, mapper);
+
+    final expectedResponseJson = [dayData];
+    final expectedResponseParsed = Mapper().mapJsonList(
+      expectedResponseJson,
+      (json) => ApiPrayerTime.fromJson(json),
+    );
+
+    dio.whenAnyGet().thenAnswer((_) => Future.value(Response(
+          data: expectedResponseJson,
+          requestOptions: RequestOptions(path: ''),
+        )));
+
+    final result = await api.getPrayerTime(PrayerTimeFilter.TODAY);
+
+    expect(result, expectedResponseParsed);
+
+    verify(dio.get(
+      any,
+      queryParameters: argThat(
+        equals({"filter": "today"}),
+        named: "queryParameters",
+      ),
+      options: anyNamed("options"),
+      cancelToken: anyNamed("cancelToken"),
+      onReceiveProgress: anyNamed("onReceiveProgress"),
+    ));
+  });
+
+  test(
+      "when McndApi called with (MONTH, YEAR) filter it adds it to the arguments and receives correct result (unwrapped)",
+      () async {
+    final dio = MockDio();
+    final mapper = MockMapper();
+    final api = McndApi(dio, mapper);
+
+    final expectedResponseJson = [
+      [dayData, dayData, dayData, dayData, dayData]
+    ];
+    final expectedResponseParsed = Mapper().mapJsonList(
+      expectedResponseJson.first,
+      (json) => ApiPrayerTime.fromJson(json),
+    );
+
+    dio.whenAnyGet().thenAnswer((_) => Future.value(Response(
+          data: expectedResponseJson,
+          requestOptions: RequestOptions(path: ''),
+        )));
+
+    final resultMonth = await api.getPrayerTime(PrayerTimeFilter.MONTH);
+    expect(resultMonth, expectedResponseParsed);
+
+    final resultYear = await api.getPrayerTime(PrayerTimeFilter.YEAR);
+    expect(resultYear, expectedResponseParsed);
+
+    verify(dio.get(
+      any,
+      queryParameters: argThat(
+        equals({"filter": "month"}),
+        named: "queryParameters",
+      ),
+      options: anyNamed("options"),
+      cancelToken: anyNamed("cancelToken"),
+      onReceiveProgress: anyNamed("onReceiveProgress"),
+    ));
+
+    verify(dio.get(
+      any,
+      queryParameters: argThat(
+        equals({"filter": "year"}),
+        named: "queryParameters",
+      ),
+      options: anyNamed("options"),
+      cancelToken: anyNamed("cancelToken"),
+      onReceiveProgress: anyNamed("onReceiveProgress"),
+    ));
+  });
+}
+
+const dayData = {
+  "d_date": "2021-04-01",
+  "fajr_begins": "05:19:00",
+  "fajr_jamah": "05:49:00",
+  "sunrise": "06:58:00",
+  "zuhr_begins": "13:31:00",
+  "zuhr_jamah": "13:45:00",
+  "asr_mithl_1": "17:02:00",
+  "asr_mithl_2": "17:02:00",
+  "asr_jamah": "17:17:00",
+  "maghrib_begins": "20:01:00",
+  "maghrib_jamah": "20:09:00",
+  "isha_begins": "21:33:00",
+  "isha_jamah": "21:38:00",
+  "is_ramadan": "0",
+  "hijri_date": "",
+  "jamah_changes": {
+    "fajr_jamah": "05:47:00",
+    "asr_jamah": "17:18:00",
+    "maghrib_jamah": "20:11:00",
+    "isha_jamah": "21:40:00"
+  },
+  "hijri_date_convert": "Sha'ban 18, 1442"
+};
