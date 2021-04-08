6d653c6ea3e4e4ecb5f539c1c6dca1697906c85b
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-08 11:35:23 +0200

    fix broken test

--- test/ui/prayer_times/prayer_times_viewmodel_test.dart
+++ test/ui/prayer_times/prayer_times_viewmodel_test.dart
@@ -3,18 +3,28 @@ import 'package:mcnd_mobile/data/models/api/api_prayer_time.dart';
 import 'package:mcnd_mobile/data/models/app/prayer_time.dart';
 import 'package:mcnd_mobile/data/models/mappers/mapper.dart';
 import 'package:mcnd_mobile/data/network/mcnd_api.dart';
+import 'package:mcnd_mobile/services/local_notifications_service.dart';
 import 'package:mcnd_mobile/ui/prayer_times/prayer_times_model.dart';
 import 'package:mcnd_mobile/ui/prayer_times/prayer_times_viewmodel.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+import '../../_test_shared/api_response.dart';
 import '../prayer_times/prayer_times_viewmodel_test.mocks.dart';
 
-@GenerateMocks([McndApi, Mapper, PrayerTimesModel, ApiPrayerTime, PrayerTime])
+@GenerateMocks([
+  McndApi,
+  Mapper,
+], customMocks: [
+  MockSpec<LocalNotificationsService>(returnNullOnMissingStub: true),
+  MockSpec<PrayerTime>(returnNullOnMissingStub: true),
+  MockSpec<ApiPrayerTime>(returnNullOnMissingStub: true),
+])
 void main() {
   final api = MockMcndApi();
   final mapper = MockMapper();
-  final vm = PrayerTimesViewModel(api, mapper);
+  final localNotificationService = MockLocalNotificationsService();
+  final vm = PrayerTimesViewModel(api, mapper, localNotificationService);
 
   tearDown(() {
     reset(api);
@@ -38,14 +48,14 @@ void main() {
   });
 
   test('when api returns result state is loaded', () async {
-    final apiResult = [MockApiPrayerTime()];
+    final apiResult = [ApiPrayerTime.fromJson(apiPrayerDayResponse)];
     final mapperResult = MockPrayerTime();
 
     when(api.getPrayerTime(any)).thenAnswer(
       (_) async => apiResult,
     );
 
-    when(mapper.mapApiPrayerTime(any)).thenReturn(mapperResult);
+    when(mapper.mapApiPrayerTime(any)).thenReturn(const Mapper().mapApiPrayerTime(apiResult.first));
 
     await vm.fetchTimes();
 
