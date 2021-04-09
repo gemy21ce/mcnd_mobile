0c6b34ab30bcd5b6cfec77bf6a54f7d60aa79621
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-09 17:49:30 +0200

    add static UI for settings page

--- lib/ui/mcnd_router.dart
+++ lib/ui/mcnd_router.dart
@@ -3,6 +3,9 @@ import 'package:mcnd_mobile/ui/home/home_screen.dart';
 import 'package:mcnd_mobile/ui/settings/settings_screen.dart';
 
 @MaterialAutoRouter(
-  routes: [AutoRoute<void>(page: HomeScreen, initial: true), AutoRoute<void>(page: SettingsScreen)],
+  routes: [
+    AutoRoute<void>(page: HomeScreen, initial: true),
+    AutoRoute<void>(page: SettingsScreen),
+  ],
 )
 class $McndRouter {}
--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -13,6 +13,144 @@ class SettingsScreen extends StatelessWidget {
           minFontSize: 15,
         ),
       ),
+      body: ListView(
+        children: const [
+          SettingsHeader(text: 'Azan Notifications'),
+          SettingsItem(
+            text: 'Fajr',
+            subtext: 'Short Azan',
+          ),
+          SettingsItem(
+            text: 'Sunrise',
+            subtext: 'Silent Notification',
+          ),
+          SettingsItem(
+            text: 'Zuhr',
+            subtext: 'Short Azan',
+          ),
+          SettingsItem(
+            text: 'Asr',
+            subtext: 'Short Azan',
+          ),
+          SettingsItem(
+            text: 'Maghrib',
+            subtext: 'Short Azan',
+          ),
+          SettingsItem(
+            text: 'Isha',
+            subtext: 'Short Azan',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SettingsHeader extends StatelessWidget {
+  final String text;
+
+  const SettingsHeader({
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0).copyWith(bottom: 0),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Color(0xff818181),
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsItem extends StatelessWidget {
+  final String text;
+  final String subtext;
+
+  const SettingsItem({
+    required this.text,
+    required this.subtext,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        showDialog<void>(
+          context: context,
+          builder: (context) {
+            return StatefulBuilder(
+              builder: (context, setState) {
+                return Dialog(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+                        child: Text(
+                          'Fajr Notification',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                      RadioListTile<int>(
+                        value: 0,
+                        groupValue: 1,
+                        onChanged: (value) {},
+                        title: const Text('No notification'),
+                      ),
+                      RadioListTile<int>(
+                        value: 1,
+                        groupValue: 1,
+                        onChanged: (value) {},
+                        title: const Text('Silent notification'),
+                      ),
+                      RadioListTile<int>(
+                        value: 0,
+                        groupValue: 1,
+                        onChanged: (value) {},
+                        title: const Text('Short Azan'),
+                      ),
+                      RadioListTile<int>(
+                        value: 0,
+                        groupValue: 1,
+                        onChanged: (value) {},
+                        title: const Text('Full Azan'),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ButtonBar(
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text('Ok'),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text('Cancel'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 24),
+        title: Text(text),
+        subtitle: Text(subtext),
+      ),
     );
   }
 }
