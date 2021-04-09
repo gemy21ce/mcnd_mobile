df8c97c347c0d266cf4ab93d732266b380bca9d7
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-09 16:31:21 +0200

    add --no-fail-on-severe

--- tools/model_generator.sh
+++ tools/model_generator.sh
@@ -4,5 +4,5 @@ if [ -z "$build_runner_mode" ]
 then
   build_runner_mode="build"
 fi
-echo ">>>Running packages pub run build_runner $build_runner_mode --delete-conflicting-outputs"
-flutter packages pub run build_runner "$build_runner_mode" --delete-conflicting-outputs
+echo ">>>Running packages pub run build_runner $build_runner_mode --delete-conflicting-outputs --no-fail-on-severe"
+flutter packages pub run build_runner "$build_runner_mode" --delete-conflicting-outputs --no-fail-on-severe
