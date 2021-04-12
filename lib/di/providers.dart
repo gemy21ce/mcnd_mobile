ffed53885aaf7770450056d1529d1efbaf6cb461
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-12 12:02:19 +0200

    add autoDispose to viewmodel providers

--- lib/di/providers.dart
+++ lib/di/providers.dart
@@ -3,10 +3,10 @@ import 'package:mcnd_mobile/di/injector.dart';
 import 'package:mcnd_mobile/ui/prayer_times/prayer_times_viewmodel.dart';
 import 'package:mcnd_mobile/ui/settings/settings_viewmodel.dart';
 
-final prayerTimesViewModelProvider = StateNotifierProvider<PrayerTimesViewModel>((ref) {
+final prayerTimesViewModelProvider = StateNotifierProvider.autoDispose<PrayerTimesViewModel>((ref) {
   return injector.get();
 });
 
-final settingsViewModelProvider = StateNotifierProvider<SettingsViewModel>((ref) {
+final settingsViewModelProvider = StateNotifierProvider.autoDispose<SettingsViewModel>((ref) {
   return injector.get();
 });
