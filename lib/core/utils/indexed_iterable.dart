693f0a3dc4cfc71bd533336758b657925e2cab44
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-12 11:37:43 +0200

    Add docs to IndexedIterable

--- lib/core/utils/indexed_iterable.dart
+++ lib/core/utils/indexed_iterable.dart
@@ -1,4 +1,5 @@
 extension IndexedIterable<E> on Iterable<E> {
+  /// mapIndexed is the same as map but with index passed in as a parameter
   Iterable<T> mapIndexed<T>(T Function(int i, E e) f) {
     var i = 0;
     return map((e) => f(i++, e));
