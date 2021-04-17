6e1b16aa20ff0bef09e1ca84d8744cba06d8db78
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-17 02:27:15 +0200

    news service

--- lib/data/models/mappers/api_news_post_mapper.dart
+++ lib/data/models/mappers/api_news_post_mapper.dart
@@ -2,7 +2,7 @@ import 'package:mcnd_mobile/data/models/api/api_news_post.dart';
 import 'package:mcnd_mobile/data/models/app/news_post.dart';
 import 'package:mcnd_mobile/data/models/mappers/mapper.dart';
 
-mixin ApiNewsPostMapper on Mapper {
+mixin ApiNewsPostMapper {
   NewsPost mapApiNewsPost(ApiNewsPost apiNewsPost) {
     return NewsPost(
       title: apiNewsPost.title.rendered,
