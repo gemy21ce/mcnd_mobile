4d4c6814d4e1771eb227b111df2424a264f75446
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-17 13:04:10 +0200

    page view

--- lib/di/providers.dart
+++ lib/di/providers.dart
@@ -3,7 +3,7 @@ import 'package:mcnd_mobile/di/injector.dart';
 import 'package:mcnd_mobile/ui/prayer_times/prayer_times_viewmodel.dart';
 import 'package:mcnd_mobile/ui/settings/settings_viewmodel.dart';
 
-final prayerTimesViewModelProvider = StateNotifierProvider.autoDispose<PrayerTimesViewModel>((ref) {
+final prayerTimesViewModelProvider = StateNotifierProvider<PrayerTimesViewModel>((ref) {
   return injector.get();
 });
 
