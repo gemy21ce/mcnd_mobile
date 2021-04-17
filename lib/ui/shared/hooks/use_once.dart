5320527ce9a290b34f0b68298411a7b08d115dbf
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-17 19:25:20 +0200

    modify use one hook

--- lib/ui/shared/hooks/use_once.dart
+++ lib/ui/shared/hooks/use_once.dart
@@ -1,5 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-void useOnce(Function function) => useEffect(() {
-      function();
-    }, []);
+void useOnce(Function function) => use(_OnceHook(function));
+
+class _OnceHook extends Hook<void> {
+  final Function function;
+
+  const _OnceHook(this.function);
+
+  @override
+  __OnceHookState createState() => __OnceHookState();
+}
+
+class __OnceHookState extends HookState<void, _OnceHook> {
+  @override
+  void initHook() {
+    super.initHook();
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      hook.function();
+    });
+  }
+
+  @override
+  void build(BuildContext context) {}
+}
