96efa40c76cfec72461499388c207ba283191fb2
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-20 14:20:41 +0200

    add open arg

--- tools/test_with_coverage.sh
+++ tools/test_with_coverage.sh
@@ -3,4 +3,6 @@ set -e
 flutter test --coverage
 lcov --remove coverage/lcov.info '**/*.g.dart' '**/*.freezed.dart' '**/*.config.dart' '**/*.mocks.dart' 'lib/gen/**' '**/*.gr.dart' -o coverage/new_lcov.info
 genhtml coverage/new_lcov.info --output=coverage
-open coverage/index.html
+if [ "$1" == "open" ]; then
+  open coverage/index.html
+fi
