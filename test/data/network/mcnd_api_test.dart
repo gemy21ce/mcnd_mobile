93b128c2cc0a8bf75496c3a4a7333d2ee0abbee2
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-20 14:17:44 +0200

    mcnd_api_test.dart

--- test/data/network/mcnd_api_test.dart
+++ test/data/network/mcnd_api_test.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mcnd_mobile/data/models/api/api_featured_media.dart';
+import 'package:mcnd_mobile/data/models/api/api_news_post.dart';
 import 'package:mcnd_mobile/data/models/api/api_prayer_time.dart';
 import 'package:mcnd_mobile/data/models/api/prayer_time_filter.dart';
 import 'package:mcnd_mobile/data/models/mappers/mapper.dart';
@@ -26,82 +28,125 @@ extension _WhenDio on MockDio {
 
 @GenerateMocks([Mapper, Dio])
 void main() {
-  test('when McndApi called with TODAY filter it adds it to the arguments and receives correct result', () async {
+  group('getPrayerTime', () {
+    test('when McndApi called with TODAY filter it adds it to the arguments and receives correct result', () async {
+      final dio = MockDio();
+      final api = McndApi(dio);
+
+      final expectedResponseJson = [apiPrayerDayResponse];
+      final expectedResponseParsed =
+          expectedResponseJson.map((dynamic json) => ApiPrayerTime.fromJson(json as Map<String, dynamic>));
+
+      dio.whenAnyGet().thenAnswer((_) => Future.value(Response<dynamic>(
+            data: expectedResponseJson,
+            requestOptions: RequestOptions(path: ''),
+          )));
+
+      final result = await api.getPrayerTime(PrayerTimeFilter.today);
+
+      expect(result, expectedResponseParsed);
+
+      verify(dio.get<dynamic>(
+        any,
+        queryParameters: argThat(
+          equals({'filter': 'today'}),
+          named: 'queryParameters',
+        ),
+        options: anyNamed('options'),
+        cancelToken: anyNamed('cancelToken'),
+        onReceiveProgress: anyNamed('onReceiveProgress'),
+      ));
+    });
+
+    test(
+        'when McndApi called with (MONTH, YEAR) filter it adds it to the arguments and receives correct result (unwrapped)',
+        () async {
+      final dio = MockDio();
+      final api = McndApi(dio);
+
+      final expectedResponseJson = [
+        [
+          apiPrayerDayResponse,
+          apiPrayerDayResponse,
+          apiPrayerDayResponse,
+          apiPrayerDayResponse,
+          apiPrayerDayResponse,
+        ]
+      ];
+      final expectedResponseParsed = expectedResponseJson.first.map(
+        (dynamic json) => ApiPrayerTime.fromJson(json as Map<String, dynamic>),
+      );
+
+      dio.whenAnyGet().thenAnswer((_) => Future.value(Response<dynamic>(
+            data: expectedResponseJson,
+            requestOptions: RequestOptions(path: ''),
+          )));
+
+      final resultMonth = await api.getPrayerTime(PrayerTimeFilter.month);
+      expect(resultMonth, expectedResponseParsed);
+
+      final resultYear = await api.getPrayerTime(PrayerTimeFilter.year);
+      expect(resultYear, expectedResponseParsed);
+
+      verify(dio.get<dynamic>(
+        any,
+        queryParameters: argThat(
+          equals({'filter': 'month'}),
+          named: 'queryParameters',
+        ),
+        options: anyNamed('options'),
+        cancelToken: anyNamed('cancelToken'),
+        onReceiveProgress: anyNamed('onReceiveProgress'),
+      ));
+
+      verify(dio.get<dynamic>(
+        any,
+        queryParameters: argThat(
+          equals({'filter': 'year'}),
+          named: 'queryParameters',
+        ),
+        options: anyNamed('options'),
+        cancelToken: anyNamed('cancelToken'),
+        onReceiveProgress: anyNamed('onReceiveProgress'),
+      ));
+    });
+  });
+
+  test('getNewsPosts', () async {
     final dio = MockDio();
     final api = McndApi(dio);
-
-    final expectedResponseJson = [apiPrayerDayResponse];
-    final expectedResponseParsed =
-        expectedResponseJson.map((dynamic json) => ApiPrayerTime.fromJson(json as Map<String, dynamic>));
+    final expectedResponseJson = [apiNewsPostResponse];
+    final expectedResponseParsed = [ApiNewsPost.fromJson(expectedResponseJson.first)];
 
     dio.whenAnyGet().thenAnswer((_) => Future.value(Response<dynamic>(
           data: expectedResponseJson,
           requestOptions: RequestOptions(path: ''),
         )));
 
-    final result = await api.getPrayerTime(PrayerTimeFilter.today);
+    final result = await api.getNewsPosts();
+
+    expect(result, expectedResponseParsed);
+  });
+
+  test('getFeaturedMedia with id', () async {
+    final dio = MockDio();
+    final api = McndApi(dio);
+    const mediaId = 55;
+    final expectedResponseJson = apiMediaResponse;
+    final expectedResponseParsed = ApiFeaturedMedia.fromJson(expectedResponseJson);
+
+    dio.whenAnyGet().thenAnswer((_) => Future.value(Response<dynamic>(
+          data: expectedResponseJson,
+          requestOptions: RequestOptions(path: ''),
+        )));
+
+    final result = await api.getFeaturedMedia(mediaId);
 
     expect(result, expectedResponseParsed);
 
     verify(dio.get<dynamic>(
-      any,
-      queryParameters: argThat(
-        equals({'filter': 'today'}),
-        named: 'queryParameters',
-      ),
-      options: anyNamed('options'),
-      cancelToken: anyNamed('cancelToken'),
-      onReceiveProgress: anyNamed('onReceiveProgress'),
-    ));
-  });
-
-  test(
-      'when McndApi called with (MONTH, YEAR) filter it adds it to the arguments and receives correct result (unwrapped)',
-      () async {
-    final dio = MockDio();
-    final api = McndApi(dio);
-
-    final expectedResponseJson = [
-      [
-        apiPrayerDayResponse,
-        apiPrayerDayResponse,
-        apiPrayerDayResponse,
-        apiPrayerDayResponse,
-        apiPrayerDayResponse,
-      ]
-    ];
-    final expectedResponseParsed = expectedResponseJson.first.map(
-      (dynamic json) => ApiPrayerTime.fromJson(json as Map<String, dynamic>),
-    );
-
-    dio.whenAnyGet().thenAnswer((_) => Future.value(Response<dynamic>(
-          data: expectedResponseJson,
-          requestOptions: RequestOptions(path: ''),
-        )));
-
-    final resultMonth = await api.getPrayerTime(PrayerTimeFilter.month);
-    expect(resultMonth, expectedResponseParsed);
-
-    final resultYear = await api.getPrayerTime(PrayerTimeFilter.year);
-    expect(resultYear, expectedResponseParsed);
-
-    verify(dio.get<dynamic>(
-      any,
-      queryParameters: argThat(
-        equals({'filter': 'month'}),
-        named: 'queryParameters',
-      ),
-      options: anyNamed('options'),
-      cancelToken: anyNamed('cancelToken'),
-      onReceiveProgress: anyNamed('onReceiveProgress'),
-    ));
-
-    verify(dio.get<dynamic>(
-      any,
-      queryParameters: argThat(
-        equals({'filter': 'year'}),
-        named: 'queryParameters',
-      ),
+      argThat(endsWith(mediaId.toString())),
+      queryParameters: anyNamed('options'),
       options: anyNamed('options'),
       cancelToken: anyNamed('cancelToken'),
       onReceiveProgress: anyNamed('onReceiveProgress'),
