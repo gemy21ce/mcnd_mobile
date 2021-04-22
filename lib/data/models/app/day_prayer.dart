387d222bc13285518ed9b9174a50f4efb6eefef2
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-04-22 22:45:49 +0200

    convert to freezed

--- lib/data/models/app/day_prayer.dart
+++ lib/data/models/app/day_prayer.dart
@@ -1,14 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:mcnd_mobile/core/utils/datetime_utils.dart';
 import 'package:mcnd_mobile/data/models/app/salah.dart';
 import 'package:mcnd_mobile/data/models/app/salah_time.dart';
-import 'package:meta/meta.dart';
 
-@immutable
-class DayPrayers {
-  final DateTime date;
-  final Map<Salah, SalahTime> times;
+part 'day_prayer.freezed.dart';
 
-  const DayPrayers(this.date, this.times);
+@freezed
+class DayPrayers with _$DayPrayers {
+  const factory DayPrayers(
+    DateTime date,
+    Map<Salah, SalahTime> times,
+  ) = _DayPrayers;
 }
 
 extension DayPrayersListExt on List<DayPrayers> {
