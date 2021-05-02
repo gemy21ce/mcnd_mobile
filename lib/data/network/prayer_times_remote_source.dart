bd111d3e6ac42b187a225e4d2895f09313d3b430
Gamal Dawood <gemy21ce@gmail.com>
2021-05-02 23:23:40 +0100

    - set the time left to next Azan instead of next Iqamah - reading next month time table if less than 10 days left in the current month data

--- lib/data/network/prayer_times_remote_source.dart
+++ lib/data/network/prayer_times_remote_source.dart
@@ -19,10 +19,21 @@ class PrayerTimesRemoteSource {
     return (await _api.getPrayerTime(PrayerTimeFilter.month)).map((e) => _mapper.mapApiPrayerTime(e)).toList();
   }
 
+  Future<List<DayPrayers>> fetchNextMonthPrayers(int month) async {
+    return (await _api.getPrayerTime(PrayerTimeFilter.year))
+        .where((e) => e.date.month == month)
+        .map((e) => _mapper.mapApiPrayerTime(e))
+        .toList();
+  }
+
   Future<List<DayPrayers>> fetchAheadPrayers(int daysAhead) async {
     final DateTime nowDate = _clock.now();
 
     final List<DayPrayers> monthDailyPrayers = await fetchThisMonthPrayers();
+
+    if (nowDate.day > 20) {
+      monthDailyPrayers.addAll(await fetchNextMonthPrayers((nowDate.month % 12) + 1));
+    }
 
     final startIndex = monthDailyPrayers.findIndexForDate(nowDate);
     final endIndex = min(startIndex + daysAhead, monthDailyPrayers.length);
